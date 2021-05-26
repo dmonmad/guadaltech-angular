3d7758780bc613522cd9ab6e4f0ac71ca0ac8a0f
imnightm4re <david.bechkam@hotmail.com>
2021-05-27 01:02:41 +0200

    build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
-};
+  production: true,
+}
+
+export const apiRoutes = {
+  apiBase: 'https://guadaltech-fullstack.herokuapp.com/api',
+  becarios: '/becarios',
+}
